a71cd5064b2f09af9659972bd8d861716507697b
findusl <dev@findusl.de>
2025-08-28 21:54:56 +0200

    initial commit

---
+++ composeApp/src/commonTest/kotlin/de/lehrbaum/abapp/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package de.lehrbaum.abapp
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/de/lehrbaum/abapp/MainViewController.kt
@@ -0,0 +1,5 @@
+package de.lehrbaum.abapp
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
